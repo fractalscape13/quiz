5d8405e551069d77ad19b0bb97fb82a118cd59ab
Joseph Wangemann <fractalscape13@gmail.com>
2019-01-24 23:15:42 -0800

    create smoother toggle transitions, cursor pointer

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
   $("#startbutton").click(function(event) {
   $(".popup").hide();
-  $("#q1").slideToggle();
+  $("#q1").fadeToggle();
   });
 
   $("#b1").click(function(event) {
-    $("#q1").slideToggle();
-    $("#q2").slideToggle();
+    $("#q1").hide();
+    $("#q2").fadeToggle();
   });
   $("#b2").click(function(event) {
-    $("#q2").slideToggle();
-    $("#q3").slideToggle();
+    $("#q2").hide();
+    $("#q3").fadeToggle();
   });
   $("#b3").click(function(event) {
-    $("#q3").slideToggle();
-    $("#q4").slideToggle();
+    $("#q3").hide();
+    $("#q4").fadeToggle();
   });
   $("#b4").click(function(event) {
-    $("#q4").slideToggle();
-    $("#q5").slideToggle();
+    $("#q4").hide();
+    $("#q5").fadeToggle();
   });
 
   $("#theQuiz").submit(function(event) {
@@ -29,13 +29,13 @@ $(document).ready(function() {
    var a4 = parseInt($("input:radio[name=fourthQuestion]:checked").val());
    var a5 = parseInt($("input:radio[name=fifthQuestion]:checked").val());
    var result = a1 + a2 + a3 + a4 + a5;
-   $("#q5").slideToggle();
+   $("#q5").hide();
    $(".output").fadeToggle();
-   randomFunction(result);
+   outputFunction(result);
   });
 });
 
-function  randomFunction(result)  {
+function  outputFunction(result)  {
    if (result >= 4) {
     $(".resultA").show();
   } else if (result === 3 || result === 2){
